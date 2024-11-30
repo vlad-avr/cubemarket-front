9d4dc45159b60cf8fc744fa88434a5dedb200c07
Kobzar Stanislav <kobzar_stanislav@knu.ua>
2024-12-01 01:28:53 +0200

    fix cors

--- back/src/main.js
+++ back/src/main.js
@@ -16,20 +16,8 @@ const fastify = Fastify({
   }
 }).withTypeProvider()
 
-fastify.options('*', async (req, rep) => {
-  rep.status(204)
-    .header('Access-Control-Allow-Origin', '*')
-    .header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-    .header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    .header('Access-Control-Allow-Credentials', 'true')
-    .send();
-});
-
 fastify.register(fastifyCors, {
   origin: '*', // Allow all origins (use with caution)
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
 });
 
 fastify.register(Swagger, { prefix: '/docs' });
